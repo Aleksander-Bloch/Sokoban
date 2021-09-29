8812e46a1981d1bf4d7926e0ca2f79127d820ac3
Aleksander Bloch <alekbloch21@gmail.com>
2021-09-29 23:06:58 +0200

    Add load, write and free functionality

--- sokoban.c
+++ sokoban.c
@@ -16,25 +16,87 @@ typedef struct Plansza {
     wiersz* wiersz;
 }plansza;
 
-void iniPlansza(plansza* p) {
+void iniPlansza(plansza *p) {
     p -> rozmiar = 0;
     p -> maxRozmiar = 0;
     p -> wiersz = NULL;
 }
 
-void dodajWiersze(plansza* p) {
-    (p -> maxRozmiar) = 1 + (p -> maxRozmiar) * MNOZNIK / DZIELNIK;
-    (p -> wiersz) = (wiersz*)realloc((p -> wiersz), (p -> maxRozmiar) * sizeof(wiersz));
+void iniWiersz(wiersz *w) {
+    w -> rozmiar = 0;
+    w -> maxRozmiar = 0;
+    w -> kolumna = NULL;
 }
 
-void dodajKolumny(wiersz* w) {
-    (w -> maxRozmiar) = 1 + (w -> maxRozmiar) * MNOZNIK / DZIELNIK;
-    (w -> kolumna) = (char*)realloc((w -> kolumna), (w -> maxRozmiar) * sizeof(char));
+void dodajWiersze(plansza *p) {
+    p -> maxRozmiar = 1 + (p -> maxRozmiar) * MNOZNIK / DZIELNIK;
+    p -> wiersz = (wiersz*)realloc(p -> wiersz, (p -> maxRozmiar) * sizeof(wiersz));
+}
+
+void dodajKolumny(wiersz *w) {
+    w -> maxRozmiar = 1 + (w -> maxRozmiar) * MNOZNIK / DZIELNIK;
+    w -> kolumna = (char*)realloc(w -> kolumna, (w -> maxRozmiar) * sizeof(char));
+}
+
+void wczytajPlansze(plansza *p) {
+    int znak = 0;
+    int poprzedni = 0;
+    dodajWiersze(p);
+    wiersz *obecnyWiersz = p -> wiersz;
+    iniWiersz(obecnyWiersz);
+
+    while((znak = getchar()) != '\n' || poprzedni != '\n') {
+        if(znak == '\n') {
+            (p -> rozmiar)++;
+            if(p -> rozmiar == p -> maxRozmiar) {
+                dodajWiersze(p);
+            }
+            obecnyWiersz = &(p -> wiersz[p -> rozmiar]);
+            iniWiersz(obecnyWiersz);
+        }
+        else {
+            if(obecnyWiersz -> rozmiar == obecnyWiersz -> maxRozmiar) {
+                dodajKolumny(obecnyWiersz);
+            }
+            obecnyWiersz -> kolumna[obecnyWiersz -> rozmiar] = znak;
+            (obecnyWiersz -> rozmiar)++;
+        }
+        poprzedni = znak;
+    }
+}
+
+void wypiszPlansze(plansza *p) {
+    int ileWierszy = p -> rozmiar;
+    int ileKolumn = 0;
+    wiersz *obecnyWiersz = NULL;
+
+    for(int i = 0; i < ileWierszy; ++i) {
+        obecnyWiersz = &(p -> wiersz[i]);
+        ileKolumn = obecnyWiersz -> rozmiar;
+        for(int j = 0; j < ileKolumn; ++j) {
+            printf("%c", obecnyWiersz -> kolumna[j]);
+        }
+        printf("\n");
+    }
+}
+
+void zwolnijPamiec(plansza *p) {
+    int ileWierszy = p -> rozmiar;
+    wiersz *obecnyWiersz = NULL;
+
+    for(int i = 0; i < ileWierszy; ++i) {
+        obecnyWiersz = &(p -> wiersz[i]);
+        free(obecnyWiersz -> kolumna);
+    }
+    free(p -> wiersz);
 }
 
 int main(void) {
     plansza p;
     iniPlansza(&p);
+    wczytajPlansze(&p);
+    wypiszPlansze(&p);
+    zwolnijPamiec(&p);
     
     return 0;
 }
